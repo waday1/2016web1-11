280f1a24a4665f3c148fa01adf03210d1dd8d5ee
waday1 <g1527079@st.t-kougei.ac.jp>
2016-06-20 17:34:29 +0900

    Update default.js

--- default.js
+++ default.js
@@ -4,23 +4,28 @@ var answer = parseInt(window.prompt('数あてゲーム　0～2'));
 
 var message;
 
-if(answer===number)
-{
- message='あたり';
-}
-else if(answer<number)
-{
- message='もっと大きいよ';
-}
-else if(answer>number)
-{
- message='もっと小さいよ';
-}
-else
-{
- message='0～2を入れてね';
-}
 
+while(true)
+{
+ if(answer<0||answer>2)
+ {
+  message='0～2を入れてね';
+ }
+ else if(answer===number)
+ {
+  message='あたり';
+  break;
+ }
+ else if(answer<number)
+ {
+ message='もっと大きいよ';
+ }
+ else if(answer>number)
+ {
+  message='もっと小さいよ';
+ }
+ 
+}
 document.getElementById('choice').textContent=message;
  
  
