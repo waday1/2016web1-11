ba7ff5d5481b360721454e1076219d42531f930a
waday1 <g1527079@st.t-kougei.ac.jp>
2016-06-20 17:19:58 +0900

    Update default.js

--- default.js
+++ default.js
@@ -1,4 +1,25 @@
-var result=window.confirm('あ');
+var number=Math.hloor(Math.random()+*3);
 
-document.getElementById('choise').textContent=result;
+var answer=parseInt(window.prompt('数あてゲーム　0～2'));
+
+var message;
+
+if(answer===number)
+{
+ massage='あたり';
+}
+else if(answer<number)
+{
+ massage='もっと大きいよ';
+}
+else if(answer>number)
+{
+ massage='もっと小さいよ';
+}
+else
+{
+ massage='0～2を入れてね'
+}
+
+document.getElementById('choise').textContent=message;
  
