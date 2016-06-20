7a0f17ad27de85a61dacb26b3b784e1b6cd432a0
waday1 <g1527079@st.t-kougei.ac.jp>
2016-06-20 17:23:13 +0900

    Update default.js

--- default.js
+++ default.js
@@ -1,26 +1,26 @@
 var number=Math.hloor(Math.random()+*3);
 
-var answer=parseInt(window.prompt('数あてゲーム　0～2')); 
+var answer=parseInt(window.prompt('数あてゲーム　0～2'));
 
 var message;
 
 if(answer===number)
 {
- massage='あたり';
+ message='あたり';
 }
 else if(answer<number)
 {
- massage='もっと大きいよ';
+ message='もっと大きいよ';
 }
 else if(answer>number)
 {
- massage='もっと小さいよ';
+ message='もっと小さいよ';
 }
 else
 {
- massage='0～2を入れてね'
+ message='0～2を入れてね'
 }
 
-document.getElementById('choise').textContent=message;
+document.getElementById('choice').textContent=message;
  
  
